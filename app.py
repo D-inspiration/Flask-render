ded411a158cb44c256bc55b462971e2200c9e254
Taripretei Zidein <tariphexdicon@gmail.com>
2025-10-31 17:42:36 +0100

    Enable CORS

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)  # <-- This enables CORS for all routes
 @app.route("/")
 def home():
     return render_template("index.html")
